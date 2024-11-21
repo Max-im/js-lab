d0b0ef3b4992359bbad1259b46aa00e070eb93d6
max-im <pogidaevmo@gmail.com>
2024-11-21 12:39:28 +0200

    DEV: ADD: logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,14 @@
-"use client"
+'use client'
 
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Header({amount}: {amount: number}) {
   return (
     <Box className="py-5 flex justify-between">
-      <Link href="/">TestLab</Link>
-      <span className="text-gray-500">Tasks: {amount}</span>
+      <Link href="/"><Image src="/logo.jpg" alt="TestLab" width={40} height={40} /></Link>
+      <Typography className="text-gray-500">Tasks: {amount}</Typography>
     </Box>
   );
 }
