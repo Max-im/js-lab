5e725e4297a3e98aed5c15237366d64cf02b6f2e
max-im <pogidaevmo@gmail.com>
2024-12-05 06:24:45 +0200

    DEV: ADD: basic nav

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,16 +1,29 @@
-'use client'
+'use client';
 
-import { Box } from '@mui/material';
 import Image from 'next/image';
 import Link from 'next/link';
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
-    <Box className="py-5 flex justify-between">
-      <div className='container mx-auto px-6 lg:px-20'>
-        <Link href="/" className="hidden md:block"><Image src="/logo.png" alt="JS-Lab" width={40} height={40} /></Link>
-        <Link href="/" className="block md:hidden"><Image src="/logo.png" alt="JS-Lab" width={26} height={26} /></Link>
+    <div className="py-5 flex relative z-10">
+      <div className='container mx-auto px-6 lg:px-20 flex items-center'>
+        <div className={`${styles.logo} rounded mr-3`}>
+          <Link href="/" className="hidden md:block"><Image src="/logo.png" alt="JS-Lab" width={40} height={40} /></Link>
+          <Link href="/" className="block md:hidden"><Image src="/logo.png" alt="JS-Lab" width={26} height={26} /></Link>
+        </div>
+        
+        <nav>
+          <ul className="flex items-center">
+            <li>
+              <Link className="py-2 px-1" href="/">Home</Link>
+            </li>
+            <li>
+              <Link className="py-2 px-1" href="/task">Tasks</Link>
+            </li>
+            </ul>
+        </nav>
       </div>
-    </Box>
+    </div>
   );
 }
