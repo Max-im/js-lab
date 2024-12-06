0f525c3abc79a223b74f216621f1c867f0a3fe26
max-im <pogidaevmo@gmail.com>
2024-12-06 19:22:46 +0200

    DEV: ADD: basic About styles

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import Image from 'next/image';
+import { useInView } from 'react-intersection-observer';
+import styles from './About.module.css'
 
 export default function About() {
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+  });
   return (
     <section className="p-5 bg-gray-100 mt-5 rounded-lg">
-        <h3 className="text-2xl font-bold text-gray-800 mb-4">About Us</h3>
-        <div className="grid grid-cols-2 gap-4">
-          <p className="">
-              
+        <h3 className={`text-2xl font-bold text-gray-800 mb-4`}>About Us</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <p>
+              <Image src="/deal.jpg" ref={ref} alt="Interview" width={500} height={300} className={`rounded-lg ${inView ? styles.image_animated : styles.image}`} />
           </p>
           <p className="justify">
-              Welcome to our code-checking platform, which is designed to help you prepare to programming interview and advance in your career. Be it an interview for your dream job or an urge to improve your skills, we have got your back. Dive into a vast collection of extended tasks, carefully crafted to simulate real-world challenges and put your problem-solving abilities to the test. Practice, learn, and grow with instant feedback on your solutions — all in one place.
+              <p>Our mission is to bring you up to speed with the most up-to-date skills and confidence to ace the technical interview and get you that dream job. Let us help you turn your dreams into reality!</p>
+              <p>At JS-Lab, we are committed to helping you conquer technical interviews and land your dream job. You will get all the skill sets, confidence, and preparation to stand out and achieve your goals. Let us help take a giant leap in your career!</p>
           </p>
         </div>
     </section>
