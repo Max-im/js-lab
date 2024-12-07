41917da87dd43712071bfd2afb2cb31df65cca1e
max-im <pogidaevmo@gmail.com>
2024-12-07 07:37:05 +0200

    DEV: ADD: Tasks Block links

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -8,17 +8,18 @@ export default function About() {
     triggerOnce: true,
   });
   return (
-    <section className="p-5 bg-gray-100 mt-5 rounded-lg">
-        <h3 className={`text-2xl font-bold text-gray-800 mb-4`}>About Us</h3>
+    <section ref={ref}  className="p-5 bg-gray-100 mt-5 rounded-lg relative">
+        <h3 className={`text-2xl font-bold text-gray-800 mb-4 ${inView ? styles.title_animated : styles.title}`}>About Us</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <p>
-              <Image src="/deal.jpg" ref={ref} alt="Interview" width={500} height={300} className={`rounded-lg ${inView ? styles.image_animated : styles.image}`} />
+              <Image src="/deal.jpg" alt="Interview" width={500} height={300} className={`rounded-lg ${inView ? styles.image_animated : styles.image}`} />
           </p>
-          <p className="justify">
-              <p>Our mission is to bring you up to speed with the most up-to-date skills and confidence to ace the technical interview and get you that dream job. Let us help you turn your dreams into reality!</p>
+          <div className="justify">
+              <p className="mb-3">Our mission is to bring you up to speed with the most up-to-date skills and confidence to ace the technical interview and get you that dream job. Let us help you turn your dreams into reality!</p>
               <p>At JS-Lab, we are committed to helping you conquer technical interviews and land your dream job. You will get all the skill sets, confidence, and preparation to stand out and achieve your goals. Let us help take a giant leap in your career!</p>
-          </p>
+          </div>
         </div>
+        {/* <div className={styles.decor + ' rounded-lg'}></div> */}
     </section>
   )
 }
--- components/TasksBlock/TaskBlock.tsx
+++ components/TasksBlock/TaskBlock.tsx
@@ -1,12 +1,44 @@
-"use client"
+"use client";
 
 import { FC } from 'react';
+import { useInView } from 'react-intersection-observer';
+import styles from './TaskBlock.module.css';
+import Link from 'next/link';
+import { Button } from '@mui/material';
 
 const TaskBlock: FC = () => {
-  return (
-    <section className="relative bg-gray-100 p-5 rounded-lg mt-5">
-      <h3 className="text-2xl font-bold text-gray-800 mb-4">Tasks:</h3>
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+  });
 
+  return (
+    <section ref={ref} className="relative bg-gray-100 p-5 rounded-lg mt-5 relative">
+      <h3 className={`text-2xl font-bold text-gray-800 mb-4 ${inView ? styles.title_animated : styles.title}`}>Tasks</h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div>
+            <Button className="accent hover:bg-emerald-800 text-white font-semibold py-2 sm:py-3 px-4 sm:px-6 shadow">
+              <Link href={'/task'}>Start Now</Link>
+            </Button><br />
+            or<br />
+            <h4>Choose the difficulty level:</h4>
+            <Button className="bg-white dark-accent__text hover:bg-gray-200 font-semibold py-1 px-3 mr-2 shadow">
+              <Link href="/task?level=easy" className={`${styles.link} ${styles.easy}`}>
+                Easy
+              </Link>
+            </Button>
+            <Button className="bg-white dark-accent__text hover:bg-gray-200 font-semibold py-1 px-3 mr-2 shadow">
+              <Link href="/task?level=medium" className={`${styles.link} ${styles.medium}`}>
+                Medium
+              </Link>
+            </Button>
+            <Button className="bg-white dark-accent__text hover:bg-gray-200 font-semibold py-1 px-3 shadow">
+              <Link href="/task?level=hard" className={`${styles.link} ${styles.hard}`}>
+                Hard
+              </Link>
+            </Button>
+        </div>
+      </div>
     </section>
   );
 }
